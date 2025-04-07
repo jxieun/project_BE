00defe553cafca259e4553532a627fa7d1d3ada1
J <xxrosevly@gmail.com>
2025-04-07 11:49:12 +0900

    카카오 로그인 페이지 (카카오버튼이미지 수정 필요)

--- src/main/java/com/baseballweb/auth/controller/KakaoLoginPageController.java
+++ src/main/java/com/baseballweb/auth/controller/KakaoLoginPageController.java
@@ -21,7 +21,7 @@ public class KakaoLoginPageController {
         String location = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id="+client_id+"&redirect_uri="+redirect_uri;
         model.addAttribute("location", location);
 
-        return "kakao_login";
+        return "login";
     }
 
 }
