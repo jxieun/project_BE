8ddd6a95f500e8eed17d57613cf1122c59dd3adb
J <xxrosevly@gmail.com>
2025-06-06 20:16:55 +0900

    수정

--- src/main/java/com/baseballweb/auth/domain/crawler/HitterStats.java
+++ src/main/java/com/baseballweb/auth/domain/crawler/HitterStats.java
@@ -2,12 +2,13 @@ package com.baseballweb.auth.domain.crawler;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class HitterStats {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String playerName;
